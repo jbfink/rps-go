4d664b73b466ff388ac0b680329a783d1d8df0cf
John Fink <john.fink@gmail.com>
2018-03-19 15:11:32 -0400

    Initial commit.

---
+++ rps.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Eventually this will be a Rock, Paper, Scissors in Go")
+	fmt.Println("My favourite number is", rand.Intn(10))
+
+}
