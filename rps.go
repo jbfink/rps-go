2fc4c924e145a70fe57c8f6700174447e05ef05f
John Fink <john.fink@gmail.com>
2018-03-20 15:34:32 -0400

    Guesses are made.

--- rps.go
+++ rps.go
@@ -13,7 +13,7 @@ func main() {
 	choice1 := random.Intn(3)
 	choice2 := random.Intn(3)
 	fmt.Println("Eventually this will be a Rock, Paper, Scissors in Go")
-	fmt.Println("My favourite number is", choice1)
-	fmt.Println("My second favourite number is", choice2)
+	fmt.Println("My guess is", choices[choice1])
+	fmt.Println("Your guess is", choices[choice2])
 
 }
