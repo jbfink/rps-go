713590fec5dca8262c0db7336480b7c28086577f
John Fink <john.fink@gmail.com>
2018-03-20 09:24:48 -0400

    Two variables for two choices.

--- rps.go
+++ rps.go
@@ -9,7 +9,10 @@ import (
 func main() {
 	seed := rand.NewSource(time.Now().UnixNano())
 	random := rand.New(seed)
+	choice1 := random.Intn(3)
+	choice2 := random.Intn(3)
 	fmt.Println("Eventually this will be a Rock, Paper, Scissors in Go")
-	fmt.Println("My favourite number is", random.Intn(3))
+	fmt.Println("My favourite number is", choice1)
+	fmt.Println("My second favourite number is", choice2)
 
 }
