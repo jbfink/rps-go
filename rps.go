20336bd122744fd744a042598ab4e733f17ebeb8
John Fink <john.fink@gmail.com>
2018-03-19 22:01:56 -0400

    Adding seed for actual randomness.

--- rps.go
+++ rps.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	seed := rand.NewSource(time.Now().UnixNano())
 	fmt.Println("Eventually this will be a Rock, Paper, Scissors in Go")
-	fmt.Println("My favourite number is", rand.Intn(10))
+	fmt.Println("My favourite number is", rand.Intn(99))
 
 }
