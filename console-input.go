55ce645e09802aa76304a4e4711e0a7533197f3e
John Fink <john.fink@gmail.com>
2018-03-24 22:32:20 -0400

    console input example.

---
+++ console-input.go
@@ -0,0 +1,31 @@
+/*
+from https://tutorialedge.net/golang/reading-console-input-golang/
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Simple Shell")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("hi", text) == 0 {
+			fmt.Println("hello, Yourself")
+		}
+
+	}
+
+}
